e4f75c036ea53616111e24190513b28e5a4fc0b6
Yuri Yoshida <164736470+umeyuri@users.noreply.github.com>
2024-03-27 14:42:12 +0900

    ログイン機能

--- resources/views/layouts/logged_in.blade.php
+++ resources/views/layouts/logged_in.blade.php
@@ -3,10 +3,15 @@
 @section('header')
 <header>
     <ul class="header_nav">
-        <li>投稿一覧</li>
-        <li>いいねリスト</li>
+        <li><a href="{{ route('posts.index') }}">投稿一覧</a></li>
+        <li><a href="{{ route('like.index') }}">いいねリスト</a></li>
         <li>ユーザープロフィール</li>
-        <li>ログアウト</li>
+        <li>
+            <form method="post" action="{{ route('logout') }}">
+                @csrf
+                <input type="submit" value="ログアウト">
+            </form>
+        </li>
     </ul>
 </header>
 @endsection
