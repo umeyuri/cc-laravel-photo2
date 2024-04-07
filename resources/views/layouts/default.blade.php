b9758180cc1892ec910da614d5eb0a67444de7e2
Yuri Yoshida <164736470+umeyuri@users.noreply.github.com>
2024-04-07 23:08:33 +0900

    投稿一覧のUI修正

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -27,10 +27,26 @@
             margin-bottom: 40px;
         }
         .post_body {
-            width: 100px;
+            width: 200px;
+        }
+        .post_body_img {
+            /* border: solid 0.5px #777777;
+            border-radius: 20px; */
+            height: 100%;
         }
         .post_body_img img{
             width: 100%;
+            /* height: 100%; */
+            border: solid 0.1px #777777;
+            border-radius: 20px;
+        }
+        .container {
+            display: grid;
+            width: 800px;
+            height: 300px;
+            grid-template-columns:  1fr 1fr 1fr 1fr;
+            grid-template-rows: 1fr 1fr 1fr 1fr;
+            gap: 10px 20px;
         }
     </style>
 </head>
